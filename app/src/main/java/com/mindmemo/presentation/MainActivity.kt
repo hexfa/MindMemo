40db9a7ad7d43270f5702efd447fad21ce3fc3ba
@hexfa <amir.dehdarian2009@gmail.com>
2023-07-02 16:11:15 +0330

    Creating packages in the project according to the Clean Architecture:

--- app/src/main/java/com/mindmemo/presentation/MainActivity.kt
+++ app/src/main/java/com/mindmemo/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mindmemo
+package com.mindmemo.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mindmemo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
