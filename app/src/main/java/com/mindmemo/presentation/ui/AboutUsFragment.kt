969f02ac02964000708be38dbff3d0588ee2b146
@hexfa <amir.dehdarian2009@gmail.com>
2023-07-05 15:05:37 +0330

    Add HomeFragment

--- app/src/main/java/com/mindmemo/presentation/ui/AboutUsFragment.kt
+++ app/src/main/java/com/mindmemo/presentation/ui/AboutUsFragment.kt
@@ -1,5 +1,15 @@
 package com.mindmemo.presentation.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mindmemo.R
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
 /**
  * A simple [Fragment] subclass.
  * Use the [AboutUsFragment.newInstance] factory method to
